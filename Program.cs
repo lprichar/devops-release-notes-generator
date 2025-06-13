ee316e616d99bedc0ec42f53fc3063a1ee45beda
Lee Richardson <github@leerichardson.com>
2025-06-12 21:17:33 -0400

    Apply-PrintPat

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Configuration;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-    
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var pat = config["pat"];
+
+if (string.IsNullOrWhiteSpace(pat))
+{
+    Console.WriteLine("No PAT found in user-secrets under the key 'pat'.");
+}
+else
+{
+    Console.WriteLine($"PAT from user-secrets: {pat}");
+}
