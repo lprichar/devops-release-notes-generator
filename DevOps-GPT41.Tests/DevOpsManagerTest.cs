383cc5e112944294208b3b7a701372a2e59b93a8
Lee Richardson <github@leerichardson.com>
2025-06-25 10:34:07 -0400

    Add a test that sets today to be 1/3/2025 at 12 PM and the last successful build at 1/1/2025 at 1 PM and asserts that GetPullRequests is called with a from of 1/1/2025 at 1 PM and a to of null

--- DevOps-GPT41.Tests/DevOpsManagerTest.cs
+++ DevOps-GPT41.Tests/DevOpsManagerTest.cs
@@ -26,6 +26,34 @@ public class DevOpsManagerTest
         result.ShouldBeNull();
     }
 
+    [Fact]
+    public async Task GivenOldBuild_WhenGenerateReleaseNotesJson_ThenGetPullRequestsCalledWithNullTo()
+    {
+        // Arrange
+        var today = new DateTime(2025, 1, 3, 12, 0, 0, DateTimeKind.Utc);
+        var lastBuild = new DateTime(2025, 1, 1, 13, 0, 0, DateTimeKind.Utc);
+        var mockDateTimeProvider = new Mock<IDateTimeProvider>();
+        mockDateTimeProvider.Setup(x => x.UtcNow).Returns(today);
+
+        var mockConnection = new Mock<IConnection>();
+        mockConnection.Setup(x => x.GetLastTwoProductionDeployments(It.IsAny<string>(), It.IsAny<string>()))
+            .ReturnsAsync(( (DateTime?)null, (DateTime?)lastBuild ));
+        mockConnection.Setup(x => x.GetPullRequests(It.IsAny<string>(), It.IsAny<DateTime>(), It.IsAny<DateTime?>()))
+            .ReturnsAsync(new List<Pr>());
+
+        var configData = MakeConfigurationData();
+        var manager = new DevOpsManager(mockDateTimeProvider.Object, mockConnection.Object, configData);
+
+        // Act
+        await manager.GenerateReleaseNotesJson();
+
+        // Assert
+        mockConnection.Verify(x => x.GetPullRequests(
+            configData.Repo,
+            lastBuild,
+            null), Times.Once);
+    }
+
     private static ConfigurationData MakeConfigurationData(
         string repo = "repo",
         string org = "org",
